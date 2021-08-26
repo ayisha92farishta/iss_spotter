76c925382b47a3d1eb8062a517fe5190ae4ac83c
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-08-26 01:02:12 +0000

    implemented fetchMyIP whereby the app returns an IP using a callback. Also implemented fetchCoordsByIP whereby the resulting coords gets returned via callback

---
+++ index.js
@@ -0,0 +1,31 @@
+// index.js
+const { fetchMyIP, fetchCoordsByIP,fetchISSFlyOverTimes } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP("142.122.102.205",(error, coordinates) => {
+//   if(error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   } 
+
+//   console.log('It worked! Returned coordinates:' , coordinates)
+// });
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
